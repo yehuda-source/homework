394fdbadead0177b306851573e54123215e4cbba
yehuda-source <ylgewirtz@gmail.com>
2026-08-18 18:06:41 -0400

    added js folder and homework 59

---
+++ js/59/scripts/index.js
@@ -0,0 +1,77 @@
+'use strict';
+//a
+  //1
+function multiply(x, y){
+    return x * y;
+}
+console.log(multiply(5,5));
+console.log(multiply(3,7));
+   //2
+function GetMultiplier(x,y){
+    return function(){
+        return x * y;
+    }
+}
+const multiplier = GetMultiplier(8,7);
+console.log(multiplier());
+//3
+function GetMultiplier2(x){
+    return function(y){
+        return x * y;
+    }
+}
+const multiplierThirteen = GetMultiplier2(13);
+console.log(multiplierThirteen(5));
+console.log(multiplierThirteen(10));
+
+
+//b
+//1
+function myEvery(array, func){
+    for(var i = 0; i < array.length; i++){
+        if(!func(array[i])){
+            return false;
+        }
+    }
+    return true;
+}
+var letters = ["a", "b", "c",];
+function isUpper(str){
+    if(str === str.toUpperCase()){
+        return true;
+    }
+    return false;
+}
+function isLower(str){
+    if(str === str.toLowerCase()){
+        return true
+    }
+    return false
+}
+var result = myEvery(letters, isUpper);
+console.log(result);
+result = letters.every(isUpper)
+console.log(result);
+var result2 = myEvery(letters, isLower);
+console.log(result2);
+result2 = letters.every(isLower);
+console.log(result2);
+
+//2
+function mySome(array, func){
+    for(var i = 0; i < array.length; i++){
+        if(func(array[i])){
+            return true;
+        }
+    }
+    return false;
+}
+const letters2 = ["a", "B", "c"]
+var someResult = mySome(letters2, isUpper);
+console.log(someResult);
+someResult = letters2.some(isUpper);
+console.log(someResult);
+var someResult2 = mySome(letters2, isLower);
+console.log(someResult2);
+someResult2 = letters2.some(isLower);
+console.log(someResult2);
