493d14ea7353adfad42be5b0e0aceae07cbadb10
yehuda-source <ylgewirtz@gmail.com>
2026-08-26 16:06:22 -0400

    added homework 61

---
+++ js/61/scripts/index.js
@@ -0,0 +1,26 @@
+//a
+function bankAccount(balance){ return {
+    balance: balance,
+   performTransaction(amount){
+    this.balance += amount;
+   }
+}
+}
+const acc1 = bankAccount(0);
+acc1.performTransaction(100)
+console.log(acc1.balance);
+acc1.performTransaction(-10);
+console.log(acc1.balance);
+
+//b
+function Transaction(amount){
+    this.balance += amount;
+}
+const acc2 = bankAccount(0);
+Transaction.call(acc2, 300);
+console.log(acc2.balance);
+
+//c
+const depositFifty = Transaction.bind(acc2, 50);
+depositFifty();
+console.log(acc2.balance);
